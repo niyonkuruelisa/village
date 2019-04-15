9ac66630501804362a82c6703add8e2b2f499192
Placide <wswapped@gmail.com>
2019-04-15 16:46:28 +0200

    OKay

--- app/config.php
+++ app/config.php
@@ -1,8 +1,8 @@
 <?php 
 //DB Params
 define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
+define('DB_USER','village');
+define('DB_PASS','village/123/');
 define('DB_NAME','village');
 
 //App Root
@@ -11,7 +11,7 @@ define("APPROOT", dirname(dirname(__FILE__))."/");
 //Url Root
 $host1 = "http://localhost/village/";
 $host = "/";
-define("URLROOT", $host1);
+define("URLROOT", $host);
 
 //Site Name
 define("SITENAME","E-Village");
